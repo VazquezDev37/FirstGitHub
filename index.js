4165c3f3faa9f8e872d4121c636907e8fda835fa
VazquezDev37 <isaacvazquez1119@gmail.com>
2021-08-19 17:42:03 -0500

    update array for rest

--- index.js
+++ index.js
@@ -13,5 +13,18 @@ let vegas = dest[1];
 let texas = dest[2];
 let utah = dest[3];
 
+for( let index = 0; index < dest.length; index++){
+    if (dest[index] === "dest");
+}
+
 console.log(dest);
+
+let rest =["FreeBird", "Chipotle", "Pluckers", "BrickHouse"];
+let freeBird = rest[0];
+let chipotle = rest[1];
+let pluckers = rest[2];
+let brickHouse = rest[3];
+
+console.log(rest);
+
 
